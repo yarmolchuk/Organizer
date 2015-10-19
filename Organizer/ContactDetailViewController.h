6c8b7c2c05d01a42d0c67328c56ea93228c65087
Staszoz <kaliberov.s@gmail.com>
2015-10-19 11:31:12 +0300

    navigationController add?

---
+++ Organizer/ContactDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactDetailViewController.h
+//  Organizer
+//
+//  Created by Stanislav on 19.10.15.
+//  Copyright © 2015 Stanislav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactDetailViewController : UIViewController
+
+@end
---
+++ Organizer/ContactDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  ContactDetailViewController.m
+//  Organizer
+//
+//  Created by Stanislav on 19.10.15.
+//  Copyright © 2015 Stanislav. All rights reserved.
+//
+
+#import "ContactDetailViewController.h"
+
+@implementation ContactDetailViewController
+
+@end
